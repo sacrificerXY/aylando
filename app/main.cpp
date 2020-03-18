303d66e9bed946e3488ba162d1b151323f5f520e
sacrificerXY <sacrificerxy@gmail.com>
2020-03-19 04:50:07 +0800

    Add basic World and sample tile rendering

--- app/main.cpp
+++ app/main.cpp
@@ -1,33 +1,32 @@
 #include <iostream>
 
-#include <SFML/Graphics/RenderWindow.hpp>
-#include <SFML/System/Clock.hpp>
-#include <SFML/Window/Event.hpp>
-#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics.hpp>
 
 #include <imgui.h>
 #include <imgui-SFML.h>
 
-#include <aylando/utils/Array2d.h>
+#include <aylando/game/World.h>
 
 namespace ay = aylando;
 
 int main()
 {
-    ay::utils::Array2d<int, 10, 10> arr;
-    reset(arr);
-    arr(0, 0) = 123;
-    for (int x = 0; x < arr.width; x++) {
-        for (int y = 0; y < arr.height; y++) {
-            std::cout << x << ',' << y << " = " << arr(x, y) << '\n';
-        }
-    }
-    sf::RenderWindow window(sf::VideoMode(640, 480), "ImGui + SFML = <3");
+    constexpr int tile_size = 16;
+    constexpr int tile_padding = 1;
+    ay::game::World world;
+    reset(world.tiles);
+    world.tiles(0, 0) = 3232;
+    int screen_width = world.tiles.width * tile_size;
+    int screen_height = world.tiles.height * tile_size;
+
+    sf::RenderWindow window(sf::VideoMode(screen_width, screen_height), "aylando");
     window.setFramerateLimit(60);
     ImGui::SFML::Init(window);
 
-    sf::CircleShape shape(100);
-    shape.setFillColor(sf::Color::Green);
+    sf::RectangleShape rect;
+    rect.setSize(sf::Vector2f(tile_size - 2 * tile_padding, tile_size - 2 * tile_padding));
+    rect.setOutlineColor(sf::Color::Green);
+    rect.setOutlineThickness(tile_padding);
 
     sf::Clock deltaClock;
     while (window.isOpen()) {
@@ -47,7 +46,22 @@ int main()
         ImGui::End();
 
         window.clear();
-        window.draw(shape);
+
+        for (int x = 0; x < world.tiles.width; x++) {
+            for (int y = 0; y < world.tiles.height; y++) {
+                rect.setPosition(x * tile_size, y * tile_size);
+                if (world.tiles(x, y) == 0) {
+                    rect.setFillColor(sf::Color::White);
+                }
+                else if (world.tiles(x, y) == 1) {
+                    rect.setFillColor(sf::Color::White);
+                }
+                else {
+                    rect.setFillColor(sf::Color::Magenta);
+                }
+                window.draw(rect);
+            }
+        }
         ImGui::SFML::Render(window);
         window.display();
     }
---
+++ include/aylando/game/World.h
@@ -0,0 +1,9 @@
+#include <aylando/utils/Array2d.h>
+
+namespace aylando::game
+{
+    struct World
+    {
+        utils::Array2d<int, 40, 40> tiles;
+    };
+}
